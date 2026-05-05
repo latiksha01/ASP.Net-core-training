c3f52870fcfd28cc392fcc1a4e01be29eae447d9
Latiksha Prajapat <latiksha01@gmail.com>
2026-05-05 11:26:51 +0530

    05May2026

---
+++ Week-12/05MAY2026/Case_studies/FantasyWebApp/FantasyWebApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace FantasyWebApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
